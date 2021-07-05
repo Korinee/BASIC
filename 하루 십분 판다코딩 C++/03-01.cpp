9492ff5208711c101600c6a9f6cd8e4f20887431
pgm3117 <80189711+pgm3117@users.noreply.github.com>
2021-07-05 21:59:37 +0900

    Create 03-01.cpp
    [하루 십분 판다코딩 C++]
    03 Complex Data
    - 01 배열과 char 문자열

---
+++ 하루 십분 판다코딩 C++/03-01.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main()
+{
+	// c++는 복합데이터형을 제공한다. 사용자 정의대로 새로운 데이터형을 만들 수 있다.
+	// 복합데이터형 : 기본 정수형과 부동소수점형의 조합
+	// 배열 (array) : 같은 데이터형의 집합
+	// typeName arrayName[arraySize]
+	short month[12] = { 1, 2, 3 };
+	
+	cout << month[0] << endl;
+	return 0;
+}*/
+
+int main()
+{
+	// 문자열 : 문자의 열
+	char a[] = { 'H', 'e', 'l', 'l', 'o', '\0'};
+
+	cout << a << endl;
+	return 0;
+}
