75c143e3d88a3d0d9098300be4e48f11ba5fe7ee
pgm3117 <80189711+pgm3117@users.noreply.github.com>
2021-07-06 20:11:21 +0900

    Create main.cpp
    [하루 십분 판다코딩 C++]
    08 Class and Object
    - 01 분할 컴파일

---
+++ 하루 십분 판다코딩 C++/08-01/main.cpp
@@ -0,0 +1,21 @@
+#include "struct.h"
+
+// 헤더 파일을 여러 파일에 포함시킬 때, 반드시 단 한번만을 포함시켜야 한다.
+
+// 1. 함수 원형
+// 2. #define이나 const를 사용하는 기호 상수
+// 3. 구조체 선언
+// 4. 클래스 선언
+// 5. 템플릿 선언
+// 6. 인라인 함수
+
+int main()
+{
+	MyStruct PandaCoding = {
+		"Panda",
+		26,
+	};
+
+	display(PandaCoding);
+	return 0;
+}
