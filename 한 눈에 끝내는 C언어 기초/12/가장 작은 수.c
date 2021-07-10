288015d04fc255395b76c599922e5c60e17445db
pgm3117 <80189711+pgm3117@users.noreply.github.com>
2021-07-10 18:16:26 +0900

    Create 가장 작은 수.c

---
+++ 한 눈에 끝내는 C언어 기초/12/가장 작은 수.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int arr[10] = {1032, 99, 1004, 234, 452, 8, 922, 445, 246, 2048};
+	int min = arr[0];
+
+	for(int i = 1; i < 10; i++)
+	{
+		if(min > arr[i])
+		{
+			min = arr[i];
+		}
+	}
+
+	printf("배열 안에서 가장 작은 수는 %d입니다.\n", min);
+
+	return 0;
+}
