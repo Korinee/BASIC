d692d4ef49330d3b81246afb5aa4d7aa1c15032c
GyeongMin Park <pdm3117@naver.com>
2021-07-28 11:35:51 +0900

    Create 01 프로젝트 생성.java

---
+++ 현직 개발자가 알려주는 자바 기초 문법/01 프로젝트 생성.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+	public static void main(String[] args) { // 메인 함수이며, 우선으로 실행되는 메소드
+		// TODO Auto-generated method stub
+		System.out.print("My name is "); // 로그 출력.
+		System.out.println("gyeongmin.");
+		System.out.println(":)");
+	}
+
+}
