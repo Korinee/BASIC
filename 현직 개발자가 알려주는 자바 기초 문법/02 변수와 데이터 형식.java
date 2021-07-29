10dc4e42509da9648d678dd3714b6edbb9f45646
GyeongMin Park <pdm3117@naver.com>
2021-07-29 12:13:54 +0900

    Create 02 변수와 데이터 형식.java

---
+++ 현직 개발자가 알려주는 자바 기초 문법/02 변수와 데이터 형식.java
@@ -0,0 +1,34 @@
+
+public class data {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char var1 = '박'; // 한 글자의 한글 or 영문 가능
+		String var2 = "경민"; // 여러글자의 문자열 가능
+		
+		byte var3 = -128; // -128 ~ +127까지 숫자 가능
+		short var4 = -32768; // -32768 ~ + 32767까지 숫자 가능
+		int var5 = 99999999; // -21억 ~ + 21억까지 숫자 가능
+		long var6 = 999999999; // 여러 숫자의 입력 가능
+		
+		float var7 = 0.5f; // 소수점 선언 후 뒤에 f를 까먹지 말고 붙이자!
+		double var8 = 0.000005; // 소수점 선언
+		
+		boolean isStart = false; // true or false를 입력하여 조건식에서 많이 사용합니다.
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		
+		System.out.println(var3);
+		System.out.println(var4);
+		System.out.println(var5);
+		System.out.println(var6);
+		
+		System.out.println(var7);
+		System.out.println(var8);
+		
+		System.out.println(isStart);
+	}
+
+}
