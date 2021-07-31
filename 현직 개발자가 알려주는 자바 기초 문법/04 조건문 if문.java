c1203d1f28b77b9c9a4f849b57f67b1aab838ea7
GyeongMin Park <pdm3117@naver.com>
2021-08-01 00:44:31 +0900

    Create 04 조건문 if문.java

---
+++ 현직 개발자가 알려주는 자바 기초 문법/04 조건문 if문.java
@@ -0,0 +1,22 @@
+
+public class forExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str[] = {"하나", "둘", "셋"};
+		
+		for(int i=0; i<3; i++)
+		{
+			System.out.println("Hello World !");
+		}
+		
+		System.out.println("Bye~");
+		
+		for(int i=0; i<str.length; i++)
+		{
+			System.out.println("No~");
+		}
+	}
+
+}
