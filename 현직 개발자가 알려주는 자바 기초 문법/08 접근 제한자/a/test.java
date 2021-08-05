4157fb4161afbb6b18886aa3360d875fe679431c
GyeongMin Park <pdm3117@naver.com>
2021-08-05 14:39:40 +0900

    Create test.java

---
+++ 현직 개발자가 알려주는 자바 기초 문법/08 접근 제한자/a/test.java
@@ -0,0 +1,15 @@
+package a;
+
+public class test {
+	
+	public String a = "Korinee"; // 같은 프로젝트 안에서는 사용가능
+	// default String a = "Korinee"; -> 사용불가
+	// protected String a = "Korinee"; -> 상속관계시 사용가능
+	// private String a = "Korinee"; -> 사용불가
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
