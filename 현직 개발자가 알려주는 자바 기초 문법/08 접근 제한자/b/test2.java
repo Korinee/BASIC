c21f36050509660a0233fe881b6a99823eb1c35b
GyeongMin Park <pdm3117@naver.com>
2021-08-05 14:39:57 +0900

    Create test2.java

---
+++ 현직 개발자가 알려주는 자바 기초 문법/08 접근 제한자/b/test2.java
@@ -0,0 +1,14 @@
+package b;
+
+import a.test;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		test test = new test();
+		System.out.println(test.a);
+	}
+
+}
