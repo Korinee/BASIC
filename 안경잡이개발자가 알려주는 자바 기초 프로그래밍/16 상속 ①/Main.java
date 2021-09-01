93ce295f9f1cf110c89428288098e3177c816323
GyeongMin Park <pdm3117@naver.com>
2021-09-01 19:41:13 +0900

    Create Main.java

---
+++ 안경잡이개발자가 알려주는 자바 기초 프로그래밍/16 상속 ①/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Student student1 = new Student("홍길동", 20, 175, 70, "20210708", 1, 4.5);
+		Student student2 = new Student("김철수", 21, 179, 68, "20200708", 4, 3.0);
+		student1.show();
+		student2.show();
+	}
+
+}
