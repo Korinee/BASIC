ebf34863dc4fe702f3ae72ded0f42223f03352a4
GyeongMin Park <pdm3117@naver.com>
2021-09-02 22:04:35 +0900

    Create Main.java

---
+++ 안경잡이개발자가 알려주는 자바 기초 프로그래밍/18 추상/Main.java
@@ -0,0 +1,30 @@
+
+public class Main extends Player {
+
+	public static void main(String[] args) {
+		
+		Main main = new Main();
+		main.play("NCT Dream - Hello Future");
+		main.pause();
+		main.stop();
+	}
+
+	@Override
+	void play(String songName) {
+		
+		System.out.println(songName + " 곡을 재생합니다.");
+	}
+
+	@Override
+	void pause() {
+		
+		System.out.println("곡을 일시정지합니다.");
+	}
+
+	@Override
+	void stop() {
+		
+		System.out.println("곡을 정지합니다.");
+	}
+
+}
